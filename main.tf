4f943cd8c63a71f745ca01ab7342481b2de551b7
David Eliason <davethemaker@gmail.com>
2025-02-03 14:22:09 -0800

    add folder structure. Add provider config and VSC config info to access AWS services. Add needed information to create VPC in us-west region

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# configure the aws provider
+provider "aws" {
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscodeaws"
+  region                   = "us-west-2"
+}
+
+module "vpc" {
+  vpc_name        = "contact-list-vpc"
+  cidr            = "10.16.0.0/16"
+  azs             = ["us-west-2a", "us-west-2b"]
+  public_subnets  = ["10.16.0.0/20", "10.16.16.0/20"]
+  private_subnets = ["10.16.32.0/20", "10.16.48.0/20"]
+  tags = {
+    Environment = "dev"
+    Project     = "contact-list"
+  }
+}
